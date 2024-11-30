bf05c62eb8349d24d183d1aba767551a01e00905
anitaprova <104943181+anitaprova@users.noreply.github.com>
2024-11-30 16:56:08 -0500

    added use_container_width

--- app.py
+++ app.py
@@ -105,13 +105,19 @@ if uploaded_file is not None:
 
     col1, col2 = st.columns(2)
     with col1:
-        st.image(image, clamp=True, channels='RGB', caption="Uploaded Image", use_column_width=True)
+        st.image(
+            image,
+            clamp=True,
+            channels="RGB",
+            caption="Uploaded Image",
+            use_container_width=True,
+        )
 
     with col2:
         st.image(
             predicted_image,
             clamp=True,
             caption="Predicted Image",
-            channels='RGB',
-            use_column_width=True,
+            channels="RGB",
+            use_container_width=True,
         )
